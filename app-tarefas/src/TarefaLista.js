5394b2c26f90ab8d9bea2668ed2f90c370f37e9f
Jhonatan Felix <jhonatan.felix@fatec.sp.gov.br>
2024-10-11 01:05:37 -0300

    refactor(p1-pdmn): altera o local do gerador lista
    - altera a resposabilidade do gerador de lista para a classe TarefaLista
    - aprimora a responsividade
    - altera o compenente principal para chamar a TarefaLista

--- app-tarefas/src/TarefaLista.js
+++ app-tarefas/src/TarefaLista.js
@@ -3,10 +3,20 @@ import React, { Component } from 'react'
 export default class TarefaLista extends Component {
     render(){
         return (
-            <div className="flex-grow-1">
-                <div className="text-bg-primary rounded text-center m-3 p-2">
-                    {this.props.tarefa}
-                </div>
+            <div className="row">
+                {
+                    this.props.lista[0] ? (
+                        this.props.lista.map((tarefa) => (
+                            <div className="flex-grow-1 col-sm-12 col-md-6 col-xl-4 col-xxl-3">
+                            <div className="text-bg-primary rounded text-center m-3 p-2">
+                              {tarefa}
+                            </div>
+                          </div>
+                ))
+              ) : (
+                <div className="align-items-center text-center m-2">Nenhuma tarefa criada</div>
+              )
+            }
             </div>
         )
     }
--- app-tarefas/src/index.js
+++ app-tarefas/src/index.js
@@ -22,22 +22,17 @@ class App extends React.Component {
           <h1 className="text-center m-2">Gerencimento de Tarefas</h1>
           <div className="container border mt-2 mb-2">
           
+          
           <TarefaEntrada
             obterTexto={this.obterTexto}
             lista={this.state.lista} />
+          
+          <TarefaLista
+            lista={this.state.lista} />
 
-            {
-              this.state.lista[0] ? (
-                this.state.lista.map((tarefa) => (
-                  <TarefaLista
-                    tarefa={tarefa} />
-                ))
-              ) : (
-                <div className="align-items-center text-center m-2">Nenhuma tarefa criada</div>
-              )
-            }
-          </div>
+            
         </div>
+      </div>
       )
     }
 
